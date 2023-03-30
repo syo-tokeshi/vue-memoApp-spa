4b271b15910c405da8f6bee7ff4d63067926c0c3
toke <54713809+syo-tokeshi@users.noreply.github.com>
2023-03-30 12:36:17 +0900

    メモアプリ作成開始。動作確認等

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref,reactive,onMounted } from "vue";
+import MemoForm from "./components/MemoForm.vue";
+
+const STORAGE_KEY = "vue-memoapp-spa";
+const memos = ref(JSON.parse(localStorage.getItem(STORAGE_KEY) || "[]"));
+const memo = reactive({
+  content: "",
+  isEditing: false,
+})
+</script>
+
+<template>
+  <div>neko</div>
+</template>
+
+<style scoped>
+
+</style>
